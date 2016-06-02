1324f8568c7b746f1a9d80fa0ca28915c7647ed3
brandonkylenewman <brandonkylenewman@gmail.com>
2016-06-01 21:38:38 -0700

    doh forgot all the new stuff in the face folders

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById("blog-button").onclick = function () {
+        location.href = "https://medium.com/@BrandonKNewman";
+};
+
+document.getElementById("twitter-button").onclick = function () {
+        location.href = "http://www.twitter.com/brandonknewman";
+};
+
+document.getElementById("resume-button").onclick = function () {
+        location.href = "pdf/resume.pdf";
+};
+
+
