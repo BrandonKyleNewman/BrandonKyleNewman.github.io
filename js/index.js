1fbb5b1a17b3d910ad2796c9a7fac6161063d202
Brandon Newman <Brandonkylenewman@gmail.com>
2016-09-18 15:58:02 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ document.getElementById("twitter-button").onclick = function () {
 };
 
 document.getElementById("resume-button").onclick = function () {
-        location.href = "pdf/resume.pdf";
+        location.href = "https://docs.google.com/document/d/1ptqduL-0e6805sDxf-s1tw64UHg3rwHv-F1Gx_Oyv50/edit?usp=sharing";
 };
 
 
